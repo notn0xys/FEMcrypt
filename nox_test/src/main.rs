6a14bc2eccb3b9c84e282a6639a38cc4e8d8f38c
Noxy 3 <thatguynoxxie@gmail.com>
2024-11-04 01:18:43 +0700

    teat

---
+++ nox_test/src/main.rs
@@ -0,0 +1,30 @@
+use std::clone;
+use iced::Sandbox;
+use iced::widget::{button, column, progress_bar, text, Column};
+use iced::{Element, Settings};
+struct Editor{
+}
+#[derive(Debug,Clone)]
+enum Message{}
+impl Sandbox for Editor{
+    type Message = Message;
+    fn new() -> Self{
+        Editor{}
+    }
+    fn title(&self) -> String{
+        String::from("First app")
+    }
+    fn update(&mut self,message: Message){
+        match message{
+
+        }
+    }
+    fn view(&self) -> Element<Message>{
+        text("hello word").into()
+    }
+    
+
+}
+fn main() -> iced::Result {
+	Editor::run(Settings::default())
+}
