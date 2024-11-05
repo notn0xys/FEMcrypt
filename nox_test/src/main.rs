e27bde19413ddcfce85b32a965b0cb99e37aa9df
Noxy 3 <thatguynoxxie@gmail.com>
2024-11-05 15:35:27 +0700

    finished 2 pages only need to connect sql now for signin/signup operation

--- nox_test/src/main.rs
+++ nox_test/src/main.rs
@@ -1,10 +1,13 @@
-use iced;
-use iced::futures::io::Sink;
+use iced::alignment::Vertical;
 use iced::widget::{
-    self, button, center, checkbox, column, container, keyed_column, row,
-    scrollable, text, text_input, Text,
+    self, button, center, checkbox, column, container, horizontal_space, keyed_column, row, text, text_input,vertical_space,Space
 };
-use iced::{Center, Element, Fill, Font, Subscription, Task as Command, Theme, Renderer};
+use iced::{self, Length,Center, Element, Fill, Font, Subscription, Task as Command, Theme, Renderer};
+#[derive(Default)]
+struct Signup{
+    username: String,
+    password: String
+}
 #[derive(Default)]
 struct Signin{
     username: String,
@@ -14,6 +17,7 @@ struct Signin{
 struct App{
     current_page: Page,
     signin: Signin,
+    signup: Signup,
 }
 #[derive(Default,Debug, Clone)]
 enum Page{
@@ -37,7 +41,9 @@ enum SigninPageMessage{
 }
 #[derive(Debug, Clone)]
 enum SignupPageMessage{
-    Signup
+    UpdateUser(String),
+    UpdatePassword(String),
+    Signup(String,String),
 }
 #[derive(Debug, Clone)]
 enum Message{
@@ -46,11 +52,19 @@ enum Message{
     Signin(SigninPageMessage),
     MainPage(MainPageMessage),
 }
+impl Signup {
+    fn new() -> Self{
+        Signup{
+            username: "".to_string(),
+            password:"".to_string()
+        }
+    }
+}
 impl Signin {
     fn new() -> Self{
         Signin{
-            username: "test".to_string(),
-            password:"123".to_string()
+            username: "".to_string(),
+            password:"".to_string()
         }
     }
 }
@@ -58,43 +72,58 @@ impl App{
     fn new() -> Self{
         App {
             current_page: Page::SignupPage,
-            signin: Signin::new()
+            signin: Signin::new(),
+            signup: Signup::new()
         }
     }
     fn view(&self) -> Element<Message>{
         match &self.current_page{
             Page::SigninPage => {
-            let title = text("Sign in").align_x(Center).width(Fill).size(40);
-            let username: widget::TextInput<'_, Message, Theme, Renderer> = text_input("username", &self.signin.username)
-            .id("username_signin")
-            .on_input(|input: String| Message::Signin(SigninPageMessage::UpdateUser(input)))                    
-            .padding(15)
-            .size(30)
-            .align_x(Center);
-            let password: widget::TextInput<'_, Message, Theme, Renderer> = text_input("password", &self.signin.password)
-            .id("password_signin")
-            .on_input(|input: String| Message::Signin(SigninPageMessage::UpdatePassword(input)))                    
-            .padding(15)
-            .size(30)
-            .align_x(Center);
-            let submit_b: widget::Button<'_, Message, Theme, Renderer> = button(container(text("Sign in")).align_x(Center))
-            .on_press(Message::Signin(SigninPageMessage::Info(self.signin.username.clone(), self.signin.password.clone())))
-            .padding(15);        
-            let content = column![title,username,password,submit_b]
-            .spacing(20);
-            
-
-            scrollable(container(content).center_x(Fill).padding(60)).into()
-                
+                let title = text("Sign in").align_x(Center).width(Fill).size(40);
+                let username: widget::TextInput<'_, Message, Theme, Renderer> = text_input("username", &self.signin.username)
+                .id("username_signin")
+                .on_input(|input: String| Message::Signin(SigninPageMessage::UpdateUser(input)))                    
+                .padding(15)
+                .size(30)
+                .align_x(Center);
+                let password: widget::TextInput<'_, Message, Theme, Renderer> = text_input("password", &self.signin.password)
+                .id("password_signin")
+                .on_input(|input: String| Message::Signin(SigninPageMessage::UpdatePassword(input)))                    
+                .padding(15)
+                .size(30)
+                .align_x(Center);
+                let submit_b:widget::Container<'_, Message> = container(button(container(text("Sign in")).align_x(Center))
+                .on_press(Message::Signin(SigninPageMessage::Info(self.signin.username.clone(), self.signin.password.clone())))
+                .padding(15))
+                .align_x(Center)
+                .width(Fill);
+                let signup: widget::Button<'_, Message, Theme, Renderer> = button("Create an Account").on_press(Message::SwitchPage(Page::SignupPage));
+                let track = row![horizontal_space(),signup];        
+                column![Space::new(30,30),title,username,password,submit_b,vertical_space(),track].spacing(50).padding(60).width(Fill).into()     
             }
             Page::SignupPage => {
                 
-                container(
-                    column![
-                    button("Signin?").on_press(Message::SwitchPage(Page::SigninPage)),
-                    button("Continue?").on_press(Message::SwitchPage(Page::GenKeyPage)),
-                ]
-                ).padding(10).into()
+                let title = text("Sign Up").align_x(Center).width(Fill).size(40);
+                let username: widget::TextInput<'_, Message, Theme, Renderer> = text_input("username", &self.signup.username)
+                .id("username_signup")
+                .on_input(|input: String| Message::Signup(SignupPageMessage::UpdateUser(input)))                    
+                .padding(15)
+                .size(30)
+                .align_x(Center);
+                let password: widget::TextInput<'_, Message, Theme, Renderer> = text_input("password", &self.signup.password)
+                .id("password_signup")
+                .on_input(|input: String| Message::Signup(SignupPageMessage::UpdatePassword(input)))                    
+                .padding(15)
+                .size(30)
+                .align_x(Center);
+                let submit_b:widget::Container<'_, Message> = container(button(container(text("Sign up")).align_x(Center))
+                .on_press(Message::Signup(SignupPageMessage::Signup(self.signup.username.clone(), self.signup.password.clone())))
+                .padding(15))
+                .align_x(Center)
+                .width(Fill);
+                let signin: widget::Button<'_, Message, Theme, Renderer> = button("Already have an account? Sign in").on_press(Message::SwitchPage(Page::SigninPage));
+                let track = row![horizontal_space(),signin].align_y(Vertical::Bottom);        
+                column![Space::new(30,30),title,username,password,submit_b,vertical_space(),track].spacing(50).padding(60).width(Fill).into()     
             }
             Page::MainPage => {
                 button("Log out").on_press(Message::SwitchPage(Page::SignupPage)).into()
@@ -141,10 +170,27 @@ impl App{
                     }
                  }  
             }
-            Message::Signup( message) => {
-                
+            Message::Signup(message) => {
+                match message{
+                    SignupPageMessage::Signup(user, pass) => {
+                        if user.len() == 0 ||  pass.len() == 0{
+                            println!("Unable to save data as it is empty");
+                        }
+                        self.add_to_db();
+                        self.current_page = Page::GenKeyPage
+                    }
+                    SignupPageMessage::UpdatePassword(val) => {
+                        self.signup.password = val
+                    }
+                    SignupPageMessage::UpdateUser(val) => {
+                        self.signup.username = val
+                    }
+                }
             }
         }
+    }
+    fn add_to_db(&self) {
+
     }
     fn check_data(&self) -> bool {
         true
